1b8407c307a336d0912a4b16d6585dc174c0479c
Ki-Mor <francois.botcazou@proton.me>
2025-02-12 09:49:17 +0100

    Update 00_Place-Holder.py
    blabla test

--- XECO_Communes/00_Place-Holder.py
+++ XECO_Communes/00_Place-Holder.py
@@ -1,1 +1,2 @@
 "HelloWorld - This a place holder for something something I didnt work on yet. Yeayh!"
+"Je rajoute des trucs depuis le wouaib, pour voir si je peux le pull sur ma machine. ceci est un test."
