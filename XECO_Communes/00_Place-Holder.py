4a67e3cffe44655c52f8d551030318af0d3f75ff
Ki-Mor <francois.botcazou@proton.me>
2025-02-12 16:18:51 +0100

    RAS - test test test

--- XECO_Communes/00_Place-Holder.py
+++ XECO_Communes/00_Place-Holder.py
@@ -1,2 +1,3 @@
 "HelloWorld - This a place holder for something something I didnt work on yet. Yeayh!"
 "Je rajoute des trucs depuis le wouaib, pour voir si je peux le pull sur ma machine. ceci est un test."
+"Je rajoute un autre truc depuis mon pc portable à la maison, c'est encore un test !!"
